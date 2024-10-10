84415f3c3dc4edd6e4ca3cf501c2d99c819de858
Aruj1111 <aruj.dwivedi.ahit@gmail.com>
2024-10-10 10:00:57 +0530

    Update settings.py

--- rsystem/rsystem/settings.py
+++ rsystem/rsystem/settings.py
@@ -153,8 +153,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'arujgarg4@gmail.com'  # Replace with your email
-EMAIL_HOST_PASSWORD = 'ojjm qrxd forq otua'     # Replace with your password
+EMAIL_HOST_USER = 'X'  # Replace with your email
+EMAIL_HOST_PASSWORD = 'X'     # Replace with your password
 DEFAULT_FROM_EMAIL = 'arujgarg4@gmail.com'
 
 
